9af47cfeea6bdd7d408a1f09baf8933cd7a8984f
HectorV <hectorfabiovasquez@gmail.com>
2023-01-13 17:23:02 -0500

    Added File and Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+import { Brand, CTA, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <h1>GPT-3</h1>
+      <div className="App">
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
